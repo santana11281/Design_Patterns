f3bc575a1e2695369e0b713da5378d48f9337fad
Michael Severino <michael11281@hotmail.com>
2022-11-05 17:06:34 -0400

    design patterns

--- src/main/java/Structural/01 - Adapter.java
+++ src/main/java/Structural/01 - Adapter.java
@@ -115,8 +115,6 @@ class AdapterPatternDemo {
         audioPlayer.play("mp4", "alone.mp4");
         audioPlayer.play("vlc", "far far away.vlc");
         audioPlayer.play("avi", "mind me.avi");
-
-
     }
 }
 //        Step 6
